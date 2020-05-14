b0619c2900d2f65027d4ba16b7bf11d3dfca4c4b
atul_gup@yahoo.co.uk <atul_gup@yahoo.co.uk>
2020-05-14 10:25:25 +0100

    v.0

--- middleware/advancedDataList.js
+++ middleware/advancedDataList.js
@@ -6,8 +6,52 @@ const advancedDataList = (model, AppID, populate) => async (req, res, next) => {
 
   const newConfig =
     "../NewConfig/" + AppID + "_" + req.headers.businessrole + "_config.json";
-  var config = require(newConfig);
-  console.log(config["ListFields"]);
+  var config1 = require(newConfig);
+  let config = {};
+  fl1 = {};
+  fl2 = [];
+  lf = [];
+
+  if (req.headers.mode == "BOTList") {
+    config1["ListBOTFields"]["Title"].forEach((element1) => {
+      lf.push(element1);
+    });
+    config1["ListBOTFields"]["SubTitle"].forEach((element1) => {
+      lf.push(element1);
+    });
+    config1["ListBOTFields"]["Description"].forEach((element1) => {
+      lf.push(element1);
+    });
+  }
+
+  console.log(config1);
+  switch (req.headers.mode) {
+    case "BOTList":
+      config1["FieldDef"].forEach((element) => {
+        lf.forEach((element2) => {
+          if (element["name"] == element2) {
+            fl1["name"] = element["name"];
+            fl1["SLabel"] = element["SLabel"];
+            fl2.push({ ...fl1 });
+            fl1 = {};
+          }
+        });
+      });
+
+      config["ListBOTFields"] = config1["ListBOTFields"];
+      config["FieldDef"] = fl2;
+      break;
+
+    case "BOTDetail":
+      config["Title"] = config1["Title"];
+      config["Tabs"] = config1["Tabs"];
+      config["DetailFields"] = config1["DetailFields"];
+      config["FieldDef"] = fl2;
+      break;
+    default:
+      config = config1;
+  }
+
   let query;
   // Copy req.query
   const reqQuery = { ...req.query };
@@ -30,9 +74,14 @@ const advancedDataList = (model, AppID, populate) => async (req, res, next) => {
   // Finding resource
   query = model.find(JSON.parse(queryStr));
 
-  // Select Fields
+  /*   // Select Fields
   if (req.query.select) {
     const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  } */
+
+  if (req.headers.mode == "BOTList") {
+    const fields = lf.join(" ");
     query = query.select(fields);
   }
 
