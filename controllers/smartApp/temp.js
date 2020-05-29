b64a75fa25861b958a4ab1b7188bb3039501e68c
atul_gup@yahoo.co.uk <atul_gup@yahoo.co.uk>
2020-05-29 12:16:08 +0100

    v.0

--- controllers/smartApp/temp.js
+++ controllers/smartApp/temp.js
@@ -96,6 +96,339 @@ const SUPP00028_Itm = require("../../models/smartApp/SUPP00028_Itm");
 // @route     GET /api/v1/bootcamps
 // @access    Public
 exports.temp = asyncHandler(async (req, res, next) => {
+  /// --------------------------------- ///
+  ///        Get inputs ..........
+  /// --------------------------------- ///
+  // Get App from Header
+  const BodyApp = await App.findOne({
+    applicationID: req.headers.applicationid,
+  });
+  // Get Role from the Header
+  const BusinessRole = await Role.findOne({
+    applicationID: req.headers.businessRole,
+  });
+  // Read New Config File
+  var cardConfig = getNewConfig(
+    req.headers.applicationid,
+    req.headers.businessrole
+  );
+
+  /// -------------------------------------------- ///
+  ///        Collect Field Names ..........
+  /// --------------------------------------------- ///
+  myFieldArray = [];
+  myPossValArray = [];
+  pvalObj = {};
+  pvalArr = [];
+  for (let index = 0; index < cardConfig.FieldDef.length; index++) {
+    const element1 = cardConfig.FieldDef[index].name;
+    myFieldArray.push(element1);
+  }
+  exclude_array = [
+    "appId",
+    "applicationId",
+    "user",
+    "userName",
+    "userEmail",
+    "company",
+    "companyName",
+    "branch",
+    "branchName",
+    "area",
+    "areaName",
+    "ItemData",
+    "TransLog",
+  ];
+  myFieldArray.push.apply(myFieldArray, exclude_array);
+
+  /// -------------------------------------------- ///
+  ///        Check input Fields with Config ..........
+  /// --------------------------------------------- ///
+  for (let index = 0; index < myFieldArray.length; index++) {
+    const el1 = myFieldArray[index];
+    app1 = req.headers.applicationid;
+    app2 = "GLOBAL";
+    role1 = req.headers.businessrole;
+    role2 = "ALL";
+    pVal1 = el1;
+    let query;
+    query = Possval.find(
+      {
+        PossibleValues: pVal1,
+        ApplicationID: { $in: [app1, app2] },
+        Role: { $in: [role1, role2] },
+      },
+      { _id: 0 }
+    );
+    const fields = "Value";
+    query = query.select(fields);
+    const rslt = await query;
+    if (rslt.length > 0) {
+      myPossValArray.push(el1);
+      pvalObj[el1] = rslt;
+      // Note: append only values
+      pvalArr.push(pvalObj);
+    }
+  }
+  // ---------------------
+  // App ID and Validate
+  // ---------------------
+  req.body.appId = BodyApp.id;
+  req.body.applicationId = req.headers.applicationid;
+  if (!req.body.appId) {
+    return next(new ErrorResponse(`Please provide App ID`, 400));
+  }
+  req.body.user = req.user.id;
+  req.body.userName = req.user.name;
+  req.body.userEmail = req.user.email;
+
+  // ---------------------------------
+  // Get Company Details and Validate
+  // ---------------------------------
+  const CompanyDetails = await Company.findById(req.user.company);
+  // Check if user setup has company (Pass)
+  if (!req.user.company) {
+    return next(
+      new ErrorResponse(
+        `The user with ID ${req.user.email} is not setup for any company`,
+        400
+      )
+    );
+  }
+  // Validate if user is creating documents in their own company (Pass)
+  if (req.headers.company) {
+    if (req.headers.company != CompanyDetails.id) {
+      return next(
+        new ErrorResponse(
+          `The user with ID ${req.user.email} can't create any documents for others companies`,
+          400
+        )
+      );
+    }
+  }
+  // Company validation passed, now USER company can be used!!
+  req.body.company = CompanyDetails.id;
+  req.body.companyName = CompanyDetails.companyName;
+
+  // ---------------------------------
+  // Get Branch Details and Validate
+  // ---------------------------------
+  if (req.headers.branchname) {
+    const BodyBranch = await Branch.findOne({
+      branchName: req.headers.branchname,
+      companyId: req.body.company,
+    });
+    if (BodyBranch) {
+      req.body.branch = BodyBranch.id;
+      req.body.branchName = BodyBranch.branchName;
+    }
+  }
+  // ---------------------------------
+  // Get Area Details and Validate
+  // ---------------------------------
+  if (req.headers.areaname) {
+    const BodyArea = await Area.findOne({
+      areaName: req.headers.areaname,
+      companyId: req.body.company,
+    });
+    if (BodyArea) {
+      req.body.area = BodyArea.id;
+      req.body.areaName = BodyArea.areaName;
+    }
+  }
+  // Validate if user has provided Branch details (Pass)
+  if (!req.headers.branch) {
+    if (!req.user.branch) {
+      return next(
+        new ErrorResponse(
+          `The user with ID ${req.user.email} can't create document as branch is not provided`,
+          400
+        )
+      );
+    }
+  }
+  // If user record has got Branch then validate is it matches with body Branch
+  if (req.user.branch) {
+    // if no Area in body but user has area then use it
+    if (!req.headers.branch) {
+      req.body.branch = req.user.branch;
+      // myorg.branchName = BodyBranch.branchName;
+    }
+    if (req.body.branch != req.user.branch) {
+      return next(
+        new ErrorResponse(
+          `The user with ID ${req.user.email} can't create document for other branches`,
+          400
+        )
+      );
+    }
+  }
+  // Branch validation passed, now get Branch details and set Body
+  const BranchDetails = await Branch.findById(req.headers.branch);
+  if (BranchDetails) {
+    req.body.branch = BranchDetails.branch;
+    req.body.branchName = BranchDetails.branchName;
+  }
+  if (!req.headers.area) {
+    if (!req.user.area) {
+      return next(
+        new ErrorResponse(
+          `The user with ID ${req.user.email} can't create document as business area can't be determined`,
+          400
+        )
+      );
+    }
+  }
+  // If user record has got Area then validate is it matches with body Area
+  if (req.user.area) {
+    // if no Area in body but user has area then use it
+    if (!req.headers.area) {
+      req.body.area = req.user.area;
+    }
+
+    // if body and user both have area then they should be same (Validation  - Pass)
+    if (req.body.area != req.user.area) {
+      return next(
+        new ErrorResponse(
+          `The user with ID ${req.user.email} can't create document for other business area`,
+          400
+        )
+      );
+    }
+  }
+  // Area validation passed, now get Area details and set Body
+  const AreaDetails = await Area.findById(req.headers.area);
+  if (AreaDetails) {
+    req.body.areaName = AreaDetails.areaName;
+    req.body.area = AreaDetails.id;
+  }
+
+  // Field Translation..
+
+  // Set Processing/Transaction Log
+  let pLog = {};
+  let pg1 = [];
+  pLog["Type"] = "NEW_RECORD";
+  pLog["User"] = req.body.user;
+  pLog["UserName"] = req.body.userName;
+  pLog["Status"] = req.body.Status;
+  pLog["TimeStamp"] = Date.now();
+  pLog["ID"] = req.body.ID;
+  pLog["applicationId"] = req.body.applicationId;
+  pg1.push(pLog);
+  req.body.TransLog = pg1;
+  req.body.ID = Math.floor(100000 + Math.random() * 900000);
+  //req.body.OrgData = myorg;
+  if (req.body.ItemData) {
+    for (let index = 0; index < req.body.ItemData.length; index++) {
+      const element = req.body.ItemData[index];
+      req.body.ItemData[index]["ID"] = req.body.ID;
+    }
+  }
+
+  mydata = req.body;
+
+  // Read Card Configuration for the Role (X1)
+  if (req.headers.fieldnames == "X") {
+    mydata = {};
+    mydata.appId = req.body.appId;
+    mydata.applicationId = req.body.applicationId;
+    mydata.user = req.body.user;
+    mydata.userName = req.body.userName;
+    mydata.userEmail = req.body.userEmail;
+    mydata.company = req.body.company;
+    mydata.companyName = req.body.companyName;
+    mydata.branch = req.body.branch;
+    mydata.branchName = req.body.branchName;
+    mydata.area = req.body.area;
+    mydata.areaName = req.body.areaName;
+    mydata.ItemData = req.body.ItemData;
+    mydata.TransLog = req.body.TransLog;
+    for (const key in req.body) {
+      cardConfig["FieldDef"].forEach((element1) => {
+        if (req.body.hasOwnProperty(key) & (element1["SLabel"] == key)) {
+          console.log(key);
+          console.log(element1["SLabel"]);
+          console.log(req.body[key]);
+          console.log(element1["name"]);
+          mydata[element1["name"]] = req.body[key];
+        }
+      });
+    }
+  }
+  console.log("After Trans", mydata);
+  req.body = mydata;
+
+  for (const key in req.body) {
+    if (req.body.hasOwnProperty(key)) {
+      var resField = myFieldArray.includes(key);
+
+      /// Header Validations....
+      if (resField === false && key !== "ItemData") {
+        return next(
+          new ErrorResponse(
+            `Header Field ${key} can't be used with this transaction`,
+            400
+          )
+        );
+      }
+      /// Item Validations....
+      if (key === "ItemData") {
+        myItemArray = [];
+        for (
+          let index = 0;
+          index < cardConfig.itemConfig.ItemFieldDefinition.length;
+          index++
+        ) {
+          const element2 =
+            cardConfig.itemConfig.ItemFieldDefinition[index].name;
+          myItemArray.push(element2);
+        }
+        for (const key2 in req.body.ItemData) {
+          for (const key3 in req.body.ItemData[key2]) {
+            if (req.body.ItemData[key2].hasOwnProperty(key3)) {
+              const element3 = req.body.ItemData[key2][key3];
+              var resItemField = myItemArray.includes(key3);
+              /// Item Validations....
+              if (
+                resItemField === false &&
+                key3 !== "Edit" &&
+                key3 !== "Display" &&
+                key3 !== "Delete"
+              ) {
+                return next(
+                  new ErrorResponse(
+                    `Item Field ${key3} can't be used with this transaction`,
+                    400
+                  )
+                );
+              }
+            }
+          }
+        }
+      }
+      /// -------------------------------------------- ///
+      ///        Possible value check ..........
+      /// --------------------------------------------- ///
+      // Comapre Possible Value fields and input values
+      var resPV = myPossValArray.includes(key);
+      //  console.log("PossVal1", key, ">>", resPV);
+      if (resPV === true) {
+        for (const k1 in pvalArr) {
+          //    console.log("PossValX", pvalArr[k1][key], ">>", key);
+          for (const b1 in pvalArr[k1][key]) {
+            const element = pvalArr[k1][key][b1];
+            //      console.log("Element", element["Value"]);
+          }
+          //    var chkPV = pvalArr.k1.includes(req.body.key);
+        }
+        pvalArr.forEach((element) => {
+          var resVal = myPossValArray.includes(key);
+        });
+      }
+    }
+  }
+
   mydata = {};
 
   pagination = {};
