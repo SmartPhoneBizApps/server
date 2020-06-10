96bfe747ec7e55808ff0ac159a7a40981b1f396c
atul_gup@yahoo.co.uk <atul_gup@yahoo.co.uk>
2020-06-10 16:19:46 +0100

    v.0

--- routes/utilities/dummyUpload.js
+++ routes/utilities/dummyUpload.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { dummyUpload } = require("../../utils/dummyUpload.js");
 const router = express.Router({ mergeParams: true });
 
-router.route("/").get(dummyUpload);
+router.route("/").post(dummyUpload);
 
 module.exports = router;
