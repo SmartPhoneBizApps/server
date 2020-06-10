168969de75e590a0035ce7dcf140d24213c916b6
atul_gup@yahoo.co.uk <atul_gup@yahoo.co.uk>
2020-06-10 14:03:11 +0100

    v.0

--- utils/fileupload.js
+++ utils/fileupload.js
@@ -9,24 +9,24 @@ const App = require("../models/appSetup/App");
 // @route     GET /api/v1/bootcamps
 // @access    Public
 exports.uploadFile = asyncHandler(async (req, res, next) => {
+  // Note the logic currently supports only one file at a time..
   console.log("Inside Upload...");
-  console.log(req.files);
-  //console.log(req);
-
   const file = req.files.file;
-  //console.log("File1 ", req.files.file);
-  //console.log("File2 ", req.file);
-
-  if (!req.files.file) {
-    return next(new ErrorResponse(`Please upload a file`, 400));
-  }
+  /////////////////////////////////////////////////////////////////////////
+  //   --------  Input - Validations  -------------------
+  /////////////////////////////////////////////////////////////////////////
   if (!req.files) {
     return next(new ErrorResponse(`Please upload a file`, 400));
   }
-  /*   if (!file.mimetype.startsWith("text/csv")) {
-    return next(new ErrorResponse(`Please upload an csv file(file)`, 400));
-  } */
-
+  if (!req.files.file) {
+    return next(new ErrorResponse(`No file with parameter file`, 400));
+  }
+  const header = req.files.file;
+  if (req.headers.filetype == "csv") {
+    if (header && !header.mimetype.startsWith("text/csv")) {
+      return next(new ErrorResponse(`Please upload an csv file(header)`, 400));
+    }
+  }
   if (file.size > process.env.MAX_FILE_UPLOAD) {
     return next(
       new ErrorResponse(
@@ -35,9 +35,76 @@ exports.uploadFile = asyncHandler(async (req, res, next) => {
       )
     );
   }
+  if (!req.headers.applicationid) {
+    return next(
+      new ErrorResponse(`Please provide header data applicationid`, 400)
+    );
+  }
+  if (!req.headers.businessrole) {
+    return next(
+      new ErrorResponse(`Please provide header data businessrole`, 400)
+    );
+  }
+  if (!req.headers.mode) {
+    return next(new ErrorResponse(`Please provide header data mode`, 400));
+  }
+  if (req.headers.mode != "File") {
+    if (req.headers.mode != "Record") {
+      if (req.headers.mode != "RecordItems") {
+        return next(
+          new ErrorResponse(`mode should be File or Record or RecordItems`, 400)
+        );
+      }
+    }
+  }
+  /////////////////////////////////////////////////////////////////////////
+  //   --------  App Data  -------------------
+  /////////////////////////////////////////////////////////////////////////
+  /// App Details
+  const BodyApp = await App.findOne({
+    applicationID: req.headers.applicationid,
+  });
+  var cardConfig = getNewConfig(
+    req.headers.applicationid,
+    req.headers.businessrole
+  );
+  if (req.headers.mode == "File") {
+    console.log("Mode is Record");
+    let Multiattachment = false;
+    for (let index = 0; index < cardConfig["Tabs"].length; index++) {
+      const element = cardConfig["Tabs"][index];
+      if (element["type"] == "MultiAttachments") {
+        Multiattachment = true;
+      }
+    }
+    if (!Multiattachment) {
+      return next(
+        new ErrorResponse(
+          `Attachment setup missing for App :  ${req.headers.applicationid}`,
+          400
+        )
+      );
+    }
+  }
+  if (
+    (req.headers.mode == "Record" || req.headers.mode == "RecordItems") &&
+    cardConfig["Create"] == "No"
+  ) {
+    return next(
+      new ErrorResponse(`App do not allow to upload or create new record`, 400)
+    );
+  }
+
+  console.log("All validations passed");
+
+  let mydata = {};
+  let outdata = [];
+  let myitem = {};
+  let outItem = [];
+  counter = Math.floor(10000 + Math.random() * 90000);
+
   //file.name = "abc.csv";
-  file.name = `file_${req.user.id}${path.parse(file.name).ext}`;
-  console.log("Inside Upload ", file.name);
+  file.name = `file_${req.user.id}${counter}${path.parse(file.name).ext}`;
   const filecsvFilePath = "./public/uploadFiles/" + file.name;
   const outFileName = "/uploadFiles/" + file.name;
   file.mv(`${process.env.DATA_UPLOAD_PATH}/${file.name}`, async (err) => {
@@ -45,11 +112,52 @@ exports.uploadFile = asyncHandler(async (req, res, next) => {
       console.error(err);
       return next(new ErrorResponse(`Problem with file upload`, 500));
     }
+    if (req.headers.mode == "Record" || req.headers.mode == "RecordItems") {
+      csvToJson.fieldDelimiter(",").getJsonFromCsv(filecsvFilePath);
+      let json_header = csvToJson.getJsonFromCsv(filecsvFilePath);
+      for (let index = 0; index < json_header.length; index++) {
+        for (const key in json_header[index]) {
+          cardConfig["FieldDef"].forEach((element1) => {
+            if (element1["SLabel"].replace(/\s/g, "") == key) {
+              mydata[element1["name"]] = json_header[index][key];
+            }
+          });
+        }
+        console.log("Atul");
+        mydata.appId = BodyApp.id;
+        mydata.applicationId = BodyApp.applicationID;
+        mydata.user = req.user.id;
+        mydata.userName = req.user.name;
+        mydata.userEmail = req.user.email;
+        mydata.company = req.user.company;
+        mydata.branch = req.user.branch;
+        mydata.area = req.user.area;
+        outdata.push(mydata);
+        mydata = {};
 
+        console.log("Data", outdata);
+        result = await createMultipleDocument(
+          req.headers.applicationid,
+          outdata
+        );
+        if (!result) {
+          return next(new ErrorResponse(`Problem with the file data`), 404);
+        }
+      }
+      res.status(200).json({
+        success: true,
+        fileName: outFileName,
+        fileType: file.mimetype,
+        data: result,
+      });
+    }
     res.status(200).json({
       success: true,
       fileName: outFileName,
       fileType: file.mimetype,
+      fileSize: file.size,
+      uploadedBy: "Atul Gupta",
+      version: "1",
     });
   });
 });
