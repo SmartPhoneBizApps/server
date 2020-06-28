b71ff934f438c02203fbf42382d07789d92c7883
atul_gup@yahoo.co.uk <atul_gup@yahoo.co.uk>
2020-06-28 11:56:05 +0100

    v.0

--- controllers/utilities/assignCourseUser.js
+++ controllers/utilities/assignCourseUser.js
@@ -61,40 +61,47 @@ exports.assignCourseUser = asyncHandler(async (req, res, next) => {
   if (configFrom["Controls"]["Source"]["SourceName"] == "jsonData") {
     let fn = "../.." + configFrom["Controls"]["Source"]["SourceFile"];
     var res1 = require(fn);
+    // Assign mapping values...
     for (let x = 0; x < res1["courses"].length; x++) {
       if (res1["courses"][x]["id"] == req.params.ID) {
         x1 = configFrom["Controls"]["Source"]["FieldMapping"];
-        //    Appdata = res1["courses"][x];
-        //
         for (let y = 0; y < x1.length; y++) {
           let t_image = [];
           const e2 = x1[y];
           for (const k3 in e2) {
             Appdata[k3] = res1["courses"][x][e2[k3]];
-            //       if (k3 == "Image") {
-            //    t_image.push(res1["courses"][x][e2[k3]]);
-            //   Appdata[k3] = t_image;
-            //     } else {
-            //      Appdata[k3] = res1["courses"][x][e2[k3]];
-            //       }
           }
         }
-      }
-    }
-
-    x2 = configFrom["Controls"]["Source"]["FixedValues"];
-    //    Appdata = res1["courses"][x];
-    for (let y = 0; y < x2.length; y++) {
-      const e2 = x2[y];
-      for (const k3 in e2) {
-        e2[k3] = getDateValues(e2[k3]);
-        Appdata[k3] = e2[k3];
       }
     }
   }
 
   if (configFrom["Controls"]["Source"]["SourceName"] == "mongoDB") {
     Appdata = await findOneAppData(req.params.ID, req.params.fromApp);
+    // Assign mapping values...
+    for (let x = 0; x < Appdata.length; x++) {
+      if (Appdata[x]["id"] == req.params.ID) {
+        x1 = configFrom["Controls"]["Source"]["FieldMapping"];
+        for (let y = 0; y < x1.length; y++) {
+          let t_image = [];
+          const e2 = x1[y];
+          for (const k3 in e2) {
+            Appdata[k3] = Appdata[x][e2[k3]];
+            console.log("Appdata[k3]", Appdata[k3]);
+          }
+        }
+      }
+    }
+  }
+
+  // Assign Fixed values...
+  x2 = configFrom["Controls"]["Source"]["FixedValues"];
+  for (let y = 0; y < x2.length; y++) {
+    const e2 = x2[y];
+    for (const k3 in e2) {
+      e2[k3] = getDateValues(e2[k3]);
+      Appdata[k3] = e2[k3];
+    }
   }
 
   if (!Appdata) {
