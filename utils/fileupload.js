38c61cdca1de7a349a00f4288b1c7499fb97bd7c
atul_gup@yahoo.co.uk <atul_gup@yahoo.co.uk>
2020-07-09 00:20:40 +0100

    v.0

--- utils/fileupload.js
+++ utils/fileupload.js
@@ -65,6 +65,7 @@ exports.uploadFile = asyncHandler(async (req, res, next) => {
     version = req.headers.version + 1;
   }
   let statuses = [];
+  console.log(req.body.key);
   rg01 = req.body.key.split(",");
   st1 = {};
   st1["title"] = rg01[0];
