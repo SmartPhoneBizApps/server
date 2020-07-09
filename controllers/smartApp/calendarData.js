045b53d8e88d067568fc202551e93e686e7870c5
atul_gup@yahoo.co.uk <atul_gup@yahoo.co.uk>
2020-07-09 17:49:29 +0100

    v.0

--- controllers/smartApp/calendarData.js
+++ controllers/smartApp/calendarData.js
@@ -15,7 +15,6 @@ const { readData, getTotalCount, nConfig } = require("../../modules/config2");
 // @access    Private
 exports.calendarData = asyncHandler(async (req, res, next) => {
   const application = await findOneApp(req.params.id);
-  console.log(req.params.id);
   let fn1 =
     "../../NewConfig/" +
     req.params.id +
@@ -28,29 +27,61 @@ exports.calendarData = asyncHandler(async (req, res, next) => {
   qPV = getPVQuery(req.params.id, req.headers.businessrole, pvconfig1);
   let resPV = await qPV;
 
-  if (req.params.id == "OPENSAP" || req.params.id == "EXTLEARN") {
-  } else {
-    // Get Table Schema
-    let path = "../../models/smartApp/" + req.params.id;
-    const model = require(path);
+  // Get Table Schema
+  let path = "../../models/smartApp/" + req.params.id;
+  const model = require(path);
 
-    // Get total Count
-    let query_c = getTotalCount(req.params.id, req, config1);
-    let rec = await query_c;
-    const count = rec.length;
+  // Get total Count
+  let query_c = getTotalCount(req.params.id, req, config1);
+  let rec = await query_c;
+  const count = rec.length;
 
-    let query = readData(req.params.id, req, config1);
-    let results = await query;
+  let query = readData(req.params.id, req, config1);
+  let results = await query;
 
-    console.log(count);
-    console.log(results);
+  console.log(count);
+  temp = {};
+  temp2 = [];
+  person = {};
+  person1 = {};
+  people = [];
+  let fn = "../../cards/calendarCard/calendarTemplate.json";
+  var res2 = require(fn);
+
+  for (let i = 0; i < results.length; i++) {
+    const element = results[i];
+    person["name"] = "Atul Gupta";
+    person["role"] = "Director";
+    person["pic"] =
+      "https://www.espncricinfo.com/inline/content/image/1224532.html";
+    if (element.calendar !== undefined) {
+      for (let x = 0; x < element.calendar.length; x++) {
+        temp = { ...element.calendar[x] };
+        if (element.calendar[x].title) {
+          temp["icon"] = "sap-icon://desktop-mobile";
+          temp["type"] = "Type06";
+          temp["ID"] = element.ID;
+          temp2.push(temp);
+          temp = {};
+        }
+      }
+    }
+
+    person["appointments"] = temp2;
+
+    if (temp2.length > 0) {
+      people.push(person);
+    }
+    temp2 = [];
+    person = {};
+    person1["title"] = "Training Calendar";
+    person1["startDate"] = "2020-07-09T08:00";
+    person1["people"] = people;
   }
-
-  let fn = "../../cards/calendarCard/TRAIN008_EmployeeLearn_new.json";
-  var res1 = require(fn);
-
+  let fn2 = "../../cards/calendarCard/TRAIN008_EmployeeLearn_new.json";
+  var res1 = require(fn2);
   res.status(200).json({
     success: true,
-    data: res1,
+    data: person1,
   });
 });
