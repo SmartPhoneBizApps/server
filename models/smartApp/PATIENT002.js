8bddb5ff04bc8fab861161d739c521f6f83a41b7
atul_gup@yahoo.co.uk <atul_gup@yahoo.co.uk>
2020-07-17 14:14:39 +0100

    v.0

---
+++ models/smartApp/PATIENT002.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const {
+  getRegExValidation,
+  getFixedData,
+} = require("../../modules/validations/validations");
+json = getRegExValidation("PATIENT002");
+var fixed1 = getFixedData();
+scmc = [];
+scmc.push(json);
+scmc.push(fixed1);
+const XSchema = new mongoose.Schema(scmc);
+module.exports = mongoose.model("PATIENT002", XSchema);
