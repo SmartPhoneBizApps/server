87044fbe9c79b91ab15c4d0e4b520b4f687f0fcf
atul_gup@yahoo.co.uk <atul_gup@yahoo.co.uk>
2020-07-27 10:24:45 +0100

    v.0

--- models/utilities/calfunction.js
+++ models/utilities/calfunction.js
@@ -66,19 +66,24 @@ class calFun {
 
   MUL(arr) {
     var a = 0;
-    var b = 0;
+    cnt = 1;
     arr.forEach((aa) => {
-      if (aa != undefined && !isNaN(aa) && aa != "") {
-        b = 1;
-        a *= aa;
+      if (cnt) {
+        a = aa;
+        cnt = 0;
+      } else {
+        if (aa != undefined && !isNaN(aa) && aa != "") {
+          a *= aa;
+        } else {
+          aa = 0;
+          a *= aa;
+        }
       }
     });
-    if (b) {
-      a = this.isInt(a);
-      return a;
-    }
-    return "";
+    a = this.isInt(a);
+    return a;
   }
+
   ITEMSUB(arr) {
     var a = 0;
     var b = 0;
