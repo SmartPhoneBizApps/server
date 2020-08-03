d18a2394582056e158d3418fbdeed6e083cb0185
atul_gup@yahoo.co.uk <atul_gup@yahoo.co.uk>
2020-08-03 13:50:29 +0100

    v.0

--- controllers/utilities/getjsonFile.js
+++ controllers/utilities/getjsonFile.js
@@ -2,17 +2,40 @@ const fs = require("fs");
 // @desc      Perform Encoding
 // @route     GET /api/v1/roles
 // @access    Public
-exports.getjsonFile = (req, res, next) => {
+exports.getjsonFile = async (req, res, next) => {
   let fn01 = "";
   var config = {};
   if (req.params.group == "questioner") {
     fn01 = "../../questioner/LEARNING_" + req.params.id + ".json";
+    config["questioner"] = require(fn01);
+    res.status(201).json({
+      success: true,
+      data: config,
+    });
 
-    config = require(fn01);
+    // try {
+    //   await fs.promises.access(fn01);
+    //   console.log("Success");
+    // } catch (error) {
+    //   console.log("Fail");
+    // }
+
+    // fs.access(fn01, (err) => {
+    //   if (err) {
+    //     console.error(err);
+    //     res.status(201).json({
+    //       success: false,
+    //       message: err,
+    //       data: config,
+    //     });
+    //     return;
+    //   }
+    //   console.log(fn01);
+    //   config = require(fn01);
+    //   res.status(201).json({
+    //     success: true,
+    //     data: config,
+    //   });
+    // });
   }
-
-  res.status(201).json({
-    success: true,
-    data: config,
-  });
 };
