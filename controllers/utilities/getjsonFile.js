ce8955a0d4df9a9261640d43d7946c6800e96916
atul_gup@yahoo.co.uk <atul_gup@yahoo.co.uk>
2020-08-04 10:54:23 +0100

    v.0

--- controllers/utilities/getjsonFile.js
+++ controllers/utilities/getjsonFile.js
@@ -20,3 +20,38 @@ exports.getjsonFile = async (req, res, next) => {
     data: config,
   });
 };
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// @desc      Perform Encoding
+// @route     GET /api/v1/roles
+// @access    Public
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++
+exports.updatejsonFile = async (req, res, next) => {
+  let fn01 = "";
+  var config = {};
+  if (req.params.group == "questioner") {
+    fn01 = "././questioner/LEARNING_" + req.params.id + ".json";
+    fn02 = "../../questioner/LEARNING_" + req.params.id + ".json";
+    config = require(fn02);
+    config = req.body;
+  }
+  console.log(config);
+  if (req.params.group == "examQuestions2") {
+    config["examQuestions2"] = {
+      Name: "Atul Gupta",
+      Phone: "078123123133",
+    };
+  }
+
+  fs.writeFile(fn01, JSON.stringify(config), function writeJSON(err) {
+    if (err) return console.log(err);
+    console.log(JSON.stringify(config));
+    console.log("writing to " + fn01);
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "File updated",
+    data: config,
+  });
+};
--- routes/utilities/getjsonFile.js
+++ routes/utilities/getjsonFile.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { getjsonFile } = require("../../controllers/utilities/getjsonFile.js");
+const {
+  getjsonFile,
+  updatejsonFile,
+} = require("../../controllers/utilities/getjsonFile.js");
 const router = express.Router({ mergeParams: true });
 const { protect } = require("../../middleware/auth");
 
 router.route("/:group/:id").get(protect, getjsonFile);
+router.route("/:group/:id").put(protect, updatejsonFile);
 
 module.exports = router;
