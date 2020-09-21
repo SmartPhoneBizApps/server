ae2ca8b4b32b17d8f83e247768ba123069bb4e30
atul_gup@yahoo.co.uk <atul_gup@yahoo.co.uk>
2020-09-21 15:24:09 +0100

    v.0

--- controllers/smartApp/test.js
+++ controllers/smartApp/test.js
@@ -5,102 +5,137 @@ const {
   tileCount,
 } = require("../../modules/config2");
 exports.test = asyncHandler(async (req, res, next) => {
-  let path = "../../models/smartApp/" + "HC0001";
-  let path2 = "../../models/smartApp/" + "EMP00004";
-  let path3 = "../../models/access/" + "User";
+  // Get App data
+  let App = "../../models/appSetup/App";
+  const Model_App = require(App);
+  apps = await Model_App.find({});
+  // Get App Role data
+  let Approle = "../../models/appSetup/Approle";
+  const Model_Approle = require(Approle);
+  approle = await Model_Approle.find({});
 
-  const Model = require(path);
-  const Model2 = require(path2);
-  const Model3 = require(path3);
-
-  // Get Count from mongoDB
-  c1 = await Model.countDocuments({
-    Status: "New",
+  console.log("App Setup Checks..", approle);
+  var set_app = new Set([]);
+  var set_role = new Set([]);
+  var set_approle = new Set([]);
+  approle.forEach((e2) => {
+    e2["Apps"].forEach((e3) => {
+      set_app.add(e3["applicationID"]);
+      m1 = e3["applicationID"] + "_" + e2["role"] + "_config.json";
+      set_approle.add(m1);
+      console.log(
+        e2["role"],
+        e3["applicationID"],
+        e3["descriptions"][0]["appDescription"]
+      );
+    });
+    set_role.add(e2["role"]);
   });
-  console.log("Count...DB", c1);
+  console.log(set_app, set_role, set_approle);
+  // // Loop App
 
-  // Get Count from mongoDB
-  c2 = await Model.distinct("Speciality");
-  console.log("Distinct values for Speciality...DB", c2);
+  // let path = "../../models/smartApp/" + "HC0001";
+  // let path2 = "../../models/smartApp/" + "EMP00004";
+  // let path3 = "../../models/access/" + "User";
 
-  // Aggregate(SUM) from mongoDB
-  filter = { Status: "Submitted" };
+  // const Model = require(path);
+  // const Model2 = require(path2);
+  // const Model3 = require(path3);
 
-  aggSum = await aggregateSum(req, "VendorName", "NetValue", "Ascending");
-  console.log("aggSum:", aggSum);
+  // // Get Count from mongoDB
+  // c1 = await Model.countDocuments({
+  //   Status: "New",
+  // });
+  // console.log("Count...DB", c1);
 
-  aggCount = await aggregateCount(req, "VendorName", "Descending");
-  console.log("aggCount:", aggCount);
+  // // Get Count from mongoDB
+  // c2 = await Model.distinct("Speciality");
+  // console.log("Distinct values for Speciality...DB", c2);
 
-  //Left Outer Join (Simple)
-  // Aggregate(SUM) from mongoDB
-  c4 = await Model3.aggregate([
-    //   { $match: filter },
-    {
-      $lookup: {
-        from: "empnew01",
-        localField: "email",
-        foreignField: "Email",
-        as: "EMP_Details",
-      },
-    },
-    //  { $group: { _id: ID, total: { $sum: Value } } },
-    //  { $sort: Sort },
-  ]);
+  // // Aggregate(SUM) from mongoDB
+  // filter = { Status: "Submitted" };
 
-  //Left Outer Join (with Conditions)
-  // Aggregate(SUM) from mongoDB
-  c5 = await Model3.aggregate([
-    //   { $match: filter },
-    {
-      $lookup: {
-        from: "empnew01",
-        let: { em: "$email", comp: "$company" },
-        pipeline: [
-          {
-            $match: {
-              $expr: {
-                $and: [
-                  { $eq: ["$Email", "$$em"] },
-                  { $eq: ["$company", "$$comp"] },
-                ],
-              },
-            },
-          },
-          {
-            $project: {
-              Benefits: 1,
-              FirstName: 1,
-              LastName: 1,
-              EmployeeID: 1,
-            },
-          },
-        ],
+  // aggSum = await aggregateSum(req, "VendorName", "NetValue", "Ascending");
+  // console.log("aggSum:", aggSum);
 
-        as: "newMatch",
-      },
-    },
-    //  { $group: { _id: ID, total: { $sum: Value } } },
-    //  { $sort: Sort },
-  ]);
+  // aggCount = await aggregateCount(req, "VendorName", "Descending");
+  // console.log("aggCount:", aggCount);
 
-  console.log("Lookup...", c4);
-  console.log("Lookup...", c5);
-  console.log("Smart Programming Examples...");
-  // Get Count
-  let array = [1, 2, 3, 5, 2, 8, 9, 2];
-  clt = array.filter((x) => x === 2).length; // -> 3
-  console.log("Count .... Array", clt);
-  myData = [];
+  // //Left Outer Join (Simple)
+  // // Aggregate(SUM) from mongoDB
+  // c4 = await Model3.aggregate([
+  //   //   { $match: filter },
+  //   {
+  //     $lookup: {
+  //       from: "empnew01",
+  //       localField: "email",
+  //       foreignField: "Email",
+  //       as: "EMP_Details",
+  //     },
+  //   },
+  //   //  { $group: { _id: ID, total: { $sum: Value } } },
+  //   //  { $sort: Sort },
+  // ]);
 
-  tCount = await tileCount(req);
+  // //Left Outer Join (with Conditions)
+  // // Aggregate(SUM) from mongoDB
+  // c5 = await Model3.aggregate([
+  //   //   { $match: filter },
+  //   {
+  //     $lookup: {
+  //       from: "empnew01",
+  //       let: { em: "$email", comp: "$company" },
+  //       pipeline: [
+  //         {
+  //           $match: {
+  //             $expr: {
+  //               $and: [
+  //                 { $eq: ["$Email", "$$em"] },
+  //                 { $eq: ["$company", "$$comp"] },
+  //               ],
+  //             },
+  //           },
+  //         },
+  //         {
+  //           $project: {
+  //             Benefits: 1,
+  //             FirstName: 1,
+  //             LastName: 1,
+  //             EmployeeID: 1,
+  //           },
+  //         },
+  //       ],
+
+  //       as: "newMatch",
+  //     },
+  //   },
+  //   //  { $group: { _id: ID, total: { $sum: Value } } },
+  //   //  { $sort: Sort },
+  // ]);
+
+  // console.log("Lookup...", c4);
+  // console.log("Lookup...", c5);
+  // console.log("Smart Programming Examples...");
+  // // Get Count
+  // let array = [1, 2, 3, 5, 2, 8, 9, 2];
+  // clt = array.filter((x) => x === 2).length; // -> 3
+  // console.log("Count .... Array", clt);
+  // myData = [];
+
+  // tCount = await tileCount(req);
+  // res.status(200).json({
+  //   success: true,
+  //   data: myData,
+  //   agg_count: aggCount,
+  //   agg_sum: aggSum,
+  //   lookup: c4,
+  //   lookup2: c5,
+  //   tCount: tCount,
+  // });
   res.status(200).json({
     success: true,
-    data: myData,
-    agg_count: aggCount,
-    agg_sum: aggSum,
-    lookup: c4,
-    lookup2: c5,
-    tCount: tCount,
+    apps: set_app,
+    role: set_role,
+    appRole: set_approle,
   });
 });
