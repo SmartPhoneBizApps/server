9082b3ac3f846a38b4d24d0968863cfdc58f366a
atul_gup@yahoo.co.uk <atul_gup@yahoo.co.uk>
2020-10-22 12:09:56 +0100

    v.0

--- routes/utilities/getURL.js
+++ routes/utilities/getURL.js
@@ -3,6 +3,6 @@ const { getURL } = require("../../controllers/utilities/getURL.js");
 const router = express.Router({ mergeParams: true });
 const { protect, authorize } = require("../../middleware/auth");
 
-router.route("/").get(protect, getURL);
+router.route("/").post(protect, getURL);
 
 module.exports = router;
